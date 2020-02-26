72cae2878e9b8021a1a020dda464dd9a3c40b021
Joao Marcos <joao.marcos@fs.com.br>
2020-02-26 14:09:55 -0300

    Tests on MainRouterWireframe

--- JMarvel/AppDelegate/MainRouterWireframe.swift
+++ JMarvel/AppDelegate/MainRouterWireframe.swift
@@ -17,11 +17,25 @@ class MainRouterWireframe {
     
     // MARK: - Privates
     
+    private let charactersList: CharacterListWireframe
+    private let favoritesList: FavoritesListWireframe
+    
+    init(characters: CharacterListWireframe = CharacterListWireframe(),
+         favorites: FavoritesListWireframe = FavoritesListWireframe()) {
+        self.charactersList = characters
+        self.favoritesList = favorites
+    }
+    
     private func createTabBar() -> UITabBarController {
         var viewControllers: [UIViewController] = []
         
-        viewControllers.append(charactersList())
-        viewControllers.append(favoritesList())
+        if let characters = getNavigationOfcharactersList() {
+            viewControllers.append(characters)
+        }
+        
+        if let favorites = getNavigationOffavoritesList() {
+            viewControllers.append(favorites)
+        }
         
         let tabBar = UITabBarController()
         tabBar.viewControllers = viewControllers
@@ -29,17 +43,15 @@ class MainRouterWireframe {
         return tabBar
     }
     
-    private func charactersList() -> UIViewController {
-        let wire = CharacterListWireframe()
-        let navigation = wire.getNavigation() ?? UIViewController()
-        navigation.tabBarItem = UITabBarItem(title: "Characters", image: #imageLiteral(resourceName: "iconCharacter"), selectedImage: #imageLiteral(resourceName: "iconCharacterFilled"))
+    private func getNavigationOfcharactersList() -> UINavigationController? {
+        let navigation = charactersList.getNavigation()
+        navigation?.tabBarItem = UITabBarItem(title: "Characters", image: #imageLiteral(resourceName: "iconCharacter"), selectedImage: #imageLiteral(resourceName: "iconCharacterFilled"))
         return navigation
     }
     
-    private func favoritesList() -> UIViewController {
-        let wire = FavoritesListWireframe()
-        let navigation = wire.getNavigation() ?? UIViewController()
-        navigation.tabBarItem = UITabBarItem(title: "Favorites", image: #imageLiteral(resourceName: "iconStar"), selectedImage: #imageLiteral(resourceName: "iconStarFilled"))
+    private func getNavigationOffavoritesList() -> UINavigationController? {
+        let navigation = favoritesList.getNavigation()
+        navigation?.tabBarItem = UITabBarItem(title: "Favorites", image: #imageLiteral(resourceName: "iconStar"), selectedImage: #imageLiteral(resourceName: "iconStarFilled"))
         return navigation
     }
 }
---
+++ JMarvelTests/MainRouter/MainRouterWireframeSpec.swift
@@ -0,0 +1,55 @@
+//
+//  MainRouterWireframeSpec.swift
+//  JMarvelTests
+//
+//  Created by Joao Marcos Ribeiro Araujo on 2/26/20.
+//  Copyright © 2020 JoaoMarcos. All rights reserved.
+//
+
+@testable import JMarvel
+import Nimble
+import Quick
+import UIKit
+
+class MainRouterWireframeSpec: QuickSpec {
+    override func spec() {
+        describe("CharacterListWireframe") {
+
+            let sut = MainRouterWireframe()
+            let window = UIWindow()
+            
+            sut.prepareInitial(window: window)
+            
+            it("should have a root view controller on window") {
+                let viewController = window.rootViewController
+                expect(viewController).toNot(beNil())
+            }
+
+            it("should have a tab bar controller on window") {
+                let viewController = window.rootViewController
+                expect(viewController).to(beAKindOf(UITabBarController.self))
+            }
+            
+            it("should have two view controllers on tab bar controller") {
+                let tabbar = window.rootViewController as? UITabBarController
+                expect(tabbar?.viewControllers?.count).to(equal(2))
+            }
+            
+            it("should have character list view controller as first on tab bar") {
+                let tabbar = window.rootViewController as? UITabBarController
+                let viewControllers = tabbar?.viewControllers
+                let navigation = viewControllers?.first as? UINavigationController
+                let firstVC = navigation?.viewControllers.first
+                expect(firstVC).to(beAKindOf(CharacterListView.self))
+            }
+            
+            it("should have favorite list view controller as last on tab bar") {
+                let tabbar = window.rootViewController as? UITabBarController
+                let viewControllers = tabbar?.viewControllers
+                let navigation = viewControllers?.last as? UINavigationController
+                let lastVC = navigation?.viewControllers.first
+                expect(lastVC).to(beAKindOf(FavoritesListView.self))
+            }
+        }
+    }
+}
